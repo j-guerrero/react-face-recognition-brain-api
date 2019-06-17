ef18417bc4d4388cac472d9ae260ea2aed308abe
Joshua Guerrero <jrg99654@gmail.com>
2019-06-16 17:28:47 -0800

    changing response

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req,res) => {})
+app.get('/', (req,res) => { res.send('Server Online') })
 
 app.post('/signin', signin.handleSignin(db,bcrypt));
 
