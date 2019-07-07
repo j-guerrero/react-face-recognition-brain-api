5aa4a845ef7566e9ff0ae1401cc9c48851a122c8
Josh Guerero <jrg99654@gmail.com>
2019-07-07 12:17:39 -0800

    Fixed seed.sql with actual matching hash for Test

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) values ('Tim','tim@gmail.com',5,'2018-01-01');
-INSERT into login (hash,email) values ('fakeHash', 'tim@gmail.com');
+INSERT into users (name, email, entries, joined) values ('Test','test@test.com',5,'2018-01-01');
+INSERT into login (hash,email) values ('$2a$10$CJF/kecG92YYTyj1Ohs1S.evbruyyLiYyeS53eBbGK2m3tlP7i/VK', 'test@test.com');
 
 COMMIT;
