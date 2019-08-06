bc62fc2feadafb2bb1b6f042c0afcbf79ff80fe9
Josh Guerero <jrg99654@gmail.com>
2019-08-06 13:11:38 -0800

    Commented server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
-const morgan = require('morgan');
+const knex = require('knex');				// SQL queries
 
+// ## -- Middleware -- ## 
+const bcrypt = require('bcrypt-nodejs');	// hash encryption for passwords
+const cors = require('cors');				// Cross-origin resource sharing 
+const bodyParser = require('body-parser');
+const morgan = require('morgan');			// logger for HTTP Requests
+
+// ## -- Controllers -- ##
 const register = require('./controllers/register.js');
 const signin = require('./controllers/signin.js');
 const profile = require('./controllers/profile.js');
 const image = require('./controllers/image.js');
 const auth = require('./controllers/authorization.js')
 
+// Postgrest 'DATABASE_URL' provided when running on Heroku platform
 const db = knex({
   client: 'pg',
 
@@ -18,12 +22,18 @@ const db = knex({
   
 });
 
+
+// ## -- Main Server Run -- ##
 const app = express();
 
+// ## -- Middleware -- ##
 app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('combined'));
 
+
+// ## -- Fetch Requests -- ##
+// Fetches require authenthication checks post-login
 app.get('/', (req,res) => { res.send('Server Online') })
 
 app.post('/signin', signin.signInAuthentication(db,bcrypt));
@@ -38,6 +48,8 @@ app.put('/image', auth.requireAuth, image.handleImage(db));
 
 app.post('/imageurl', auth.requireAuth, image.handleApiCall);
 
+
+// Default port is 3000; must define as env_varible @ Nodemon start if other
 app.listen(process.env.PORT || 3000, ()=>{
 
 	let port;
